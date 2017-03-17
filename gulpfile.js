ef5bec160f60e06a792c789cd57c01e1039573ac
Jenny Qian <isjia@163.com>
2017-03-17 22:06:10 +0800

    update gulp with browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var shell = require('shelljs');
 
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var browserSync = require('browser-sync').create();
 
 gulp.task('build-gitbook', function(){
   if(shell.exec('npm run build').code != 0){
@@ -15,4 +16,21 @@ gulp.task('deploy-to-gh-pages', function(){
     .pipe(ghPages());
 });
 
+// create a task that ensures the `html` task is complete before
+// reloading browsers
+gulp.task('html-watch', ['build-gitbook'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('serve', ['build-gitbook'], function(){
+  browserSync.init({
+    server: {
+      baseDir: './notes/_book'
+    }
+  });
+  gulp.watch(['./notes/**/*.md'], ['html-watch']);
+});
+
 gulp.task('publish', ['build-gitbook', 'deploy-to-gh-pages']);
+gulp.task('default', ['serve']);
